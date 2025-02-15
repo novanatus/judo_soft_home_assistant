ca308de0c0c82e9bbe0d9fec4bb3b657b0cee2be
novanatus <hasenstab.melvin@icloud.com>
2025-02-15 23:22:25 +0100

    Update __init__.py

--- custom_components/judo_isoft/__init__.py
+++ custom_components/judo_isoft/__init__.py
@@ -1,11 +1,15 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-
 from .const import DOMAIN
+from .api import JudoAPI
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Setzt die Integration in Home Assistant auf."""
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = entry.data
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = {
+        "ip": entry.data["ip"],
+        "username": entry.data["username"],
+        "password": entry.data["password"]
+    }
 
     hass.async_create_task(
         hass.config_entries.async_forward_entry_setup(entry, "sensor")
